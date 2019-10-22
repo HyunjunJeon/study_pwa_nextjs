f9d40de633ad1f9f148f0434f6385bfd6e30ba59
HyunjunJeon <jeonhj920@gmail.com>
2019-10-23 08:14:26 +0900

    antd 를 이용하여 기본 컴포넌트 생성 완료

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,29 @@ import App from "next/app";
 import React from "react";
 import withApollo from "../lib/withApollo";
 import { ApolloProvider } from "react-apollo";
+import { Layout } from "antd";
+const { Footer } = Layout;
 
 class MyApp extends App {
+    static async getInitialProps({ Component, router, ctx}){
+        let pageProps = {};
+
+        if(Component.getInitialProps){
+            pageProps = await Component.getInitialProps(ctx);
+        }
+
+        return { pageProps };
+    }
+
     render(){
         const { Component, pageProps, apollo } = this.props;
         return (
             <ApolloProvider client={apollo}>
                 <>
-                    <Component {...pageProps} />
+                    <Layout>
+                        <Component {...pageProps} />
+                        <Footer>This is Footer...</Footer>
+                    </Layout>
                 </>
             </ApolloProvider>
         );
