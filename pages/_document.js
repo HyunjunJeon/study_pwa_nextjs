ffd8aaba0dbfba62bdab37953598aeeacd749164
HyunjunJeon <jeonhj920@gmail.com>
2019-10-24 21:18:32 +0900

    manifest.json 적용, Service Worker에 대한 공부 준비!

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,8 @@ export default class MyDocument extends Document {
             <html>
                 <Head>
                     <meta name="author" content={"HyunjunJeon"}></meta>
+                    <meta name="theme-color" content={"#000"}></meta>
+                    <link rel="manifest" href="/static/manifest.json"/>
                     <link href="//cdnjs.cloudflare.com/ajax/libs/antd/3.8.1/antd.min.css" rel="stylesheet" />
                     <style>{`body {background-color: #EFF2F5!important}`}</style>
                 </Head>
